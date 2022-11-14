fd0fd53fdbc129b7d47b8aaee5ccda003f3b0003
gozdemogus <gozdemogus@hotmail.com>
2022-11-14 15:49:12 +0300

    register controller

---
+++ CrmUpSchool.UILayer/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrmUpSchool.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CrmUpSchool.UILayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        //buradaki Business katmanındaki Manager değil
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;  
+        }
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AppUser appUser)
+        {
+            return View();
+        }
+    }
+}
+
